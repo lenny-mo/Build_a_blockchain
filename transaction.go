2257cf39263dcf7737cc2422d9a065e80ee809bb
Lenny Mo <lennymo.work@gmail.com>
2023-07-08 12:05:03 +0800

    update transaction.go

--- transaction.go
+++ transaction.go
@@ -4,6 +4,12 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"fmt"
+	"strings"
+)
+
+const (
+	coinbasefee = 100
 )
 
 type Transaction struct {
@@ -13,8 +19,8 @@ type Transaction struct {
 }
 
 type TXinput struct {
-	TXid      []byte
-	Voutindex int
+	TXid      []byte // transaction id
+	Voutindex int    // index of the output
 	Signature []byte
 }
 
@@ -34,7 +40,7 @@ func (tx Transaction) Serialize() []byte {
 
 	return encoded.Bytes()
 }
-    
+
 // Hash returns the hash of the Transaction
 func (tx Transaction) Hash() []byte {
 
@@ -42,4 +48,45 @@ func (tx Transaction) Hash() []byte {
 
 	// return a slice of the hash
 	return hash[:]
+}
+
+func CoinBaseTx(toAddr string) *Transaction {
+	// coinbase transaction has no input, so we use an empty byte slice
+	// also, the index of the output is -1 which means it create output without input
+	// the signature is nil
+	txin := TXinput{[]byte{}, -1, nil}
+	// value of coinbase transaction is 100
+	txout := TXoutput{coinbasefee, []byte(toAddr)}
+	// create a transaction
+	tx := Transaction{nil, []TXinput{txin}, []TXoutput{txout}}
+	// get the hash of the transaction and set it as the ID
+	tx.ID = tx.Hash()
+
+	return &tx
+}
+
+func (tx *Transaction) String() string {
+	var lines []string
+
+	for i, input := range tx.In {
+		lines = append(lines, fmt.Sprintf("Input %d:", i))
+		lines = append(lines, fmt.Sprintf("  TXID:      %x", input.TXid))
+		lines = append(lines, fmt.Sprintf("  Out:       %d", input.Voutindex))
+		lines = append(lines, fmt.Sprintf("  Signature: %x", input.Signature))
+	}
+
+	for i, output := range tx.Out {
+		lines = append(lines, fmt.Sprintf("Output %d:", i))
+		lines = append(lines, fmt.Sprintf("  Value:  %d", output.Value))
+		lines = append(lines, fmt.Sprintf("  Script: %x", output.PublickeyHash))
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func main() {
+	tx := CoinBaseTx("test")
+
+	fmt.Println(tx.String())
+
 }
