988b590cb61fc0f74b89ef66ad1b1216312d2137
Lenny Mo <lennymo.work@gmail.com>
2023-07-11 18:20:18 +0800

    add sendtx module

--- cli.go
+++ cli.go
@@ -21,12 +21,6 @@ func (cli *CLI) validateArgs() {
 		fmt.Println("cli arg len must be greater than 1!")
 		os.Exit(1)
 	}
-
-	// print all args including args[0]
-	for i, arg := range os.Args {
-		fmt.Printf("Args[%d]: %v", i, arg)
-
-	}
 }
 
 func (cli *CLI) Run() {
@@ -36,6 +30,16 @@ func (cli *CLI) Run() {
 	// which
 	addBlock := flag.NewFlagSet("addblock", flag.ExitOnError)
 	printBlock := flag.NewFlagSet("printblock", flag.ExitOnError)
+	getBalance := flag.NewFlagSet("getbalance", flag.ExitOnError)
+	// 在 "getbalance" 这个 FlagSet 对象中定义了一个新的字符串参数 "address"。
+	// 可以通过 -address 参数来提供一个地址
+	// default value is "", usage is "The address to get balance for"
+	addr := getBalance.String("address", "", "The address to get balance for")
+
+	sendtx := flag.NewFlagSet("sendtx", flag.ExitOnError)
+	sendtxFrom := sendtx.String("from", "", "Source wallet address")
+	sendtxTo := sendtx.String("to", "", "Destination wallet address")
+	sendtxAmount := sendtx.Int("amount", 0, "Amount to send")
 
 	switch os.Args[1] {
 	//如果第一个命令行参数等于 "addblock"，那么就执行下面的代码块。
@@ -52,6 +56,18 @@ func (cli *CLI) Run() {
 			panic(err)
 		}
 
+	case "getbalance":
+		err := getBalance.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+
+	case "sendtx":
+		err := sendtx.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+
 	default:
 		fmt.Println("invalid command")
 		os.Exit(1)
@@ -63,6 +79,32 @@ func (cli *CLI) Run() {
 
 	if printBlock.Parsed() {
 		cli.printBlock()
+	}
+
+	if getBalance.Parsed() {
+		// check if the address is valid
+		if len(os.Args[2]) == 0 {
+			fmt.Println("invalid address")
+			os.Exit(1)
+		}
+		cli.GetBalance(*addr)
+	}
+
+	if sendtx.Parsed() {
+		if len(*sendtxFrom) == 0 {
+			fmt.Println("invalid sender address")
+			os.Exit(1)
+		}
+		if len(*sendtxTo) == 0 {
+			fmt.Println("invalid receiver address")
+			os.Exit(1)
+		}
+		if *sendtxAmount <= 0 {
+			fmt.Println("invalid amount")
+			os.Exit(1)
+		}
+
+		cli.SendTx(*sendtxFrom, *sendtxTo, *sendtxAmount)
 	}
 
 }
@@ -82,4 +124,23 @@ func (cli *CLI) addBlock() {
 
 func (cli *CLI) printBlock() {
 	cli.blockchain.IterateBlockchain()
+}
+
+func (cli *CLI) GetBalance(addr string) {
+	balance := 0
+	utxos := cli.blockchain.FindUTXO(addr)
+
+	for _, utxo := range utxos {
+		balance += utxo.Value
+	}
+
+	fmt.Printf("Balance of %s: %d\n", addr, balance)
+}
+
+func (cli *CLI) SendTx(from, to string, amount int) {
+	tx := CreateTransaction(from, to, amount, cli.blockchain)
+
+	cli.blockchain.AddBlock([]*Transaction{tx})
+
+	fmt.Println("Success!")
 }
